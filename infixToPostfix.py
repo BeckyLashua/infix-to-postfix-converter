91ce1d9f4b3c808e51a40b5cf9b328b87f4e0471
BeckyLashua <beckylashua@gmail.com>
2020-06-18 22:32:45 -0400

    Updated variable names

--- infixToPostfix.py
+++ infixToPostfix.py
@@ -6,78 +6,78 @@
 
 
 ### FUNCTION DESCRIPTION DO THESE ON ALL
-def runProgram():
+def run_program():
     # Get a string from the user
-    userInput = getInput()
-    
-    # Fully paranthesize expression before converting
-    paranthesizedInput = paranthesize(userInput)
-    
+    user_input = get_input()
+
+    # Fully parenthesize expression before converting
+    parenthesized_input = parenthesize(user_input)
+
     # Convert to postfix notation 
-    result = convertToPostfix(paranthesizedInput)
-    
+    result = convert_to_postfix(parenthesized_input)
+
     # Display result to user
     print(result)
-    
 
-def getInput():
+
+def get_input():
     return input("Enter an infix expression: ")
-    
+
 
 # precondition: string passed to function must be already fully paranthesized and a valid 
 # infix expression.
-def convertToPostfix(infixExpression):
-    output = ''             # this will hold the postfix expression
-    stackList = []          # will be used as a stack for non operand chars
-    notValid = "Error. This is not a valid infix expression"
-    brackets = []           # needed for validating balance in expression
-    hasOperators = False    # an infix expressio needs to have operators
-    
-    if len(infixExpression) < 3:
-        return notValid
-    
-    for letter in infixExpression: 
-        if isLeftParan(letter):
+def convert_to_postfix(infix_expression):
+    output = ''  # this will hold the postfix expression
+    stack_list = []  # will be used as a stack for non operand chars
+    not_valid = "Error. This is not a valid infix expression"
+    brackets = []  # needed for validating balance in expression
+    has_operators = False  # an infix expression needs to have operators
+
+    if len(infix_expression) < 3:
+        return not_valid
+
+    for letter in infix_expression:
+        if is_left_paran(letter):
             brackets.append(')')
-            stackList.append(letter)
-        elif isRightParan(letter):
+            stack_list.append(letter)
+        elif is_right_paran(letter):
             # first check to make sure the string is balanced so far
             if len(brackets) == 0 or brackets[-1] != ')':
-                return notValid    # no need to continue. Not valid.
-            else:   # otherwise, pop the matching bracket off the stack
+                return not_valid  # no need to continue. Not valid.
+            else:  # otherwise, pop the matching bracket off the stack
                 brackets.pop()
-            # leftParanFound = False
-            leftParanFound = False
-            # while not leftParanFound
-            while leftParanFound == False and len(stackList) != 0:
+            # left_paran_found = False
+            left_paran_found = False
+            # while not left_paran_found
+            while left_paran_found == False and len(stack_list) != 0:
                 # if i == '(', 
-                if isLeftParan(stackList[-1]):
-                    # leftParanFound = True:
-                    leftParanFound = True
+                if is_left_paran(stack_list[-1]):
+                    # left_paran_found = True:
+                    left_paran_found = True
                     # remove '(' from list
-                    stackList.pop()
+                    stack_list.pop()
                 # else  
                 else:
                     # pop from stack and add to output string 
-                    output += stackList.pop()
-        elif isOperator(letter):
-            hasOperators = True 
+                    output += stack_list.pop()
+        elif is_operator(letter):
+            has_operators = True
             # if length of stack list is 0 or top of stack is '('
-            if len(stackList) == 0 or isLeftParan(stackList[-1]):
+            if len(stack_list) == 0 or is_left_paran(stack_list[-1]):
                 # add to stack list
-                stackList.append(letter)
+                stack_list.append(letter)
             # else
             else:
                 # if end/top of stack is also an operator, 
-                if isOperator(stackList[-1]):
+                if is_operator(stack_list[-1]):
                     # compare precendence. 
-                    precedence = determinePrecedence(letter, stackList[-1])
+                    precedence = determine_precedence(letter, stack_list[-1])
                     # if top/end of stack takes precedence, 
                     if precedence == -1:
                         # remove top and add to output string
-                        output += stackList.pop()
-                        # add current letter to stackList
-                        stackList.append(letter)
+                        output += stack_list.pop()
+                        # add current letter to stack_list
+                        stack_list.append(letter)
                     # else if the current letter has precedence
                     elif precedence == 1:
                         # add current operator to output string
@@ -85,57 +85,61 @@ def convertToPostfix(infixExpression):
                     # else they have equal precedence
                     else:
                         pass
-                                                                 
-        else: 
+
+        else:
             output += letter
-    
+
     # if there are no operators in expression, it is not valid
-    if hasOperators == False:
-        return notValid
-        
+    if not has_operators:
+        return not_valid
+
     # if there is anything remaining in brackets stack, it's not balanced
     if len(brackets) != 0:
-        return notValid
-    
+        return not_valid
+
     # if stack List is not empty 
-    if len(stackList) != 0:
-        while len(stackList) != 0:
+    if len(stack_list) != 0:
+        while len(stack_list) != 0:
             # add element to output
-            output += stackList.pop()
-    
+            output += stack_list.pop()
+
     return output
-    
-def paranthesize(expression):
+
+
+def parenthesize(expression):
     # Returns an infix expression fully paranthesized. 
-    
+
     return expression
-    
-def determinePrecedence(operatorOne, operatorTwo):
+
+
+def determine_precedence(operator_one, operator_two):
     operators = {
         '^': 3,
-        '*': 2, 
-        '/': 2, 
+        '*': 2,
+        '/': 2,
         '+': 1,
         '-': 1
     }
-    
-    firstVal = operators[operatorOne]
-    secondVal = operators[operatorTwo]
-    
-    return firstVal - secondVal
 
-def isLeftParan(character):
+    first_val = operators[operator_one]
+    second_val = operators[operator_two]
+
+    return first_val - second_val
+
+
+def is_left_paran(character):
     if character == '(':
         return True
     return False
-    
-def isRightParan(character):
+
+
+def is_right_paran(character):
     if character == ')':
         return True
     return False
-    
 
-def isOperator(character):  # will likely need to convert these operators later
+
+def is_operator(character):  # will likely need to convert these operators later
     operators = ['^', '*', '/', '+', '-']
     for char in operators:
         if char == character:
@@ -143,5 +147,4 @@ def isOperator(character):  # will likely need to convert these operators later
     return False
 
 
-runProgram() 
- 
+run_program()
